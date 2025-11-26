e8750c240f27edcfe3b408c3e54adccb04ff2597
Joshua Orvis <jorvis@gmail.com>
2025-11-26 01:11:54 -0600

    Minor: removed comments referencing sh script which no longer exists

--- cgi/config.py
+++ cgi/config.py
@@ -6,12 +6,10 @@ DB_CONFIG={
     "port":3306
 }
 
-# Make sure this matches in generate_qr.sh
 BASE_URL="http://localhost"
 
 CONFERENCE_NAME="ConferenceQR Demo"
 
 ADMIN_KEY="change_me_admin_key"
 
-# Make sure this matches in generate_qr.sh
 QR_SECRET = "choose_a_very_long_random_secret"
