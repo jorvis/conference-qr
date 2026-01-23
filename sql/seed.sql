a0eebbabe03a8f291788dce3155620171f346ba6
Joshua Orvis <jorvis@gmail.com>
2026-01-22 23:02:37 -0600

    Updated seed DB with latest vendors

--- sql/seed.sql
+++ sql/seed.sql
@@ -1,7 +1,8 @@
 INSERT INTO places (code,name,type) VALUES ('S01','Presidential Symposium','session');
 INSERT INTO places (code,name,type) VALUES ('S02','General Assembly','session');
 INSERT INTO places (code,name,type) VALUES ('S03','Awards Ceremony','session');
-INSERT INTO places (code,name,type) VALUES ('E01','Belluci Lab - Creighton University','exhibitor');
+
+INSERT INTO places (code,name,type) VALUES ('E01','Creighton University - Bellucci Center','exhibitor');
 INSERT INTO places (code,name,type) VALUES ('E02','Hyperacusis Research, Limited','exhibitor');
 INSERT INTO places (code,name,type) VALUES ('E03','Interacoustics','exhibitor');
 INSERT INTO places (code,name,type) VALUES ('E04','Turner Scientific','exhibitor');
@@ -12,4 +13,12 @@ INSERT INTO places (code,name,type) VALUES ('E08','NIRx Medical Technologies, LL
 INSERT INTO places (code,name,type) VALUES ('E09','Sutter Instrument','exhibitor');
 INSERT INTO places (code,name,type) VALUES ('E10','Regeneron','exhibitor');
 INSERT INTO places (code,name,type) VALUES ('E11','Giant Tek, Inc.','exhibitor');
-INSERT INTO places (code,name,type) VALUES ('E12','gene Expression Analysis Portal (gEAR)','exhibitor');
+INSERT INTO places (code,name,type) VALUES ('E12','CILcare','exhibitor');
+INSERT INTO places (code,name,type) VALUES ('E13','Tinnitus Quest','exhibitor');
+INSERT INTO places (code,name,type) VALUES ('E14','Tucker-Davis Technologies','exhibitor');
+INSERT INTO places (code,name,type) VALUES ('E15','Universitätsklinikum Tübingen Studienzentrale Auge und Ohr','exhibitor');
+INSERT INTO places (code,name,type) VALUES ('E16','LLS ROWIAK LaserLabSolutions GmbH','exhibitor');
+INSERT INTO places (code,name,type) VALUES ('E17','American Tinnitus Association','exhibitor');
+INSERT INTO places (code,name,type) VALUES ('E18','gEAR Portal booth','exhibitor');
+INSERT INTO places (code,name,type) VALUES ('E19','iotaMotion, Inc.','exhibitor');
+
